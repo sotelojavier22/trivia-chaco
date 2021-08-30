8db65203268de0b6bee3d9ca95e481ebf746f87f
Maca <mcrnzacarias@gmail.com>
2021-08-29 21:21:07 -0300

    agrego login

--- src/trivia/quiz/urls.py
+++ src/trivia/quiz/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'quiz'
 urlpatterns = [
+    path('',RedirectView.as_view(pattern_name='quiz:ruta_iniciar')),
+    path('registrarse/', views.registrarse, name='ruta_registrarse'),
+    path('', include('django.contrib.auth.urls')),
     path('iniciar/',views.iniciar, name='ruta_iniciar'),
     path('pregunta/<int:pregunta_orden>/', views.vista_pregunta, name='ruta_pregunta'),
     path('pregunta/<int:id_pregunta>/responder',views.responder, name='ruta_responder'),
