ab29bd37626ee4d5b0cf072b733ba1277f4aefae
sotelojavier22 <sotelojavier22@gmail.com>
2021-08-30 21:55:37 -0300

    Se agrega funcionalidad para registrar usuarios

--- src/trivia/quiz/urls.py
+++ src/trivia/quiz/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('pregunta/<int:pregunta_orden>/', views.vista_pregunta, name='ruta_pregunta'),
     path('pregunta/<int:id_pregunta>/responder',views.responder, name='ruta_responder'),
     path('resultado/', views.mostrar_resultado, name='ruta_resultado'),
+    path('registro/',views.registro, name='registro'),
 ]
--- src/trivia/quiz/views.py
+++ src/trivia/quiz/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from .models import Pregunta, Opcion, Respuesta
+from .forms import CustomUserCreateForm
+from django.contrib import messages
 
 def iniciar(request):
     return HttpResponse('<a href="../pregunta/1">Iniciar</a>')
@@ -23,4 +25,18 @@ def responder(request, id_pregunta):
 def mostrar_resultado(request):
     return HttpResponse("ganaste!")
 
+def registro(request):
+    data = {
+        'form': CustomUserCreateForm()
+    }
 
+    if request.method == 'POST':
+        formulario = CustomUserCreateForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request,'Te has registrado correctamente')
+            #redirigir al home
+            #return redirect(to='iniciar')
+            return HttpResponseRedirect(reverse('quiz:ruta_iniciar'))
+        data["form"] = formulario
+    return render(request,'registration/form_registro.html',data)
